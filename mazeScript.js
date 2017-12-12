56509831e59a35af4bd4a1af2319c53ed9235418
pbuttler <peterdbuttler@gmail.com>
2017-12-11 17:02:00 -0700

    Added a randomize button to repeatedly rebuild the maze

--- mazeScript.js
+++ mazeScript.js
@@ -1,14 +1,16 @@
 var canvas = document.getElementById("mazeCanvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#43876e";
-ctx.fillRect(100,50,400,200);
 
-ctx.fillStyle = "#16112d";
-for(i = 0; i < 200; i++){
-	for(j = 0; j < 100; j++){
-		if(Math.random() > 0.5){
-			ctx.fillRect((i * 2) + 100, (j * 2) + 50, 2, 2);
-		}		
+function randomizeMaze(){
+	ctx.fillStyle = "#43876e";
+	ctx.fillRect(100,50,400,200);
+	ctx.fillStyle = "#16112d";
+	for(i = 0; i < 200; i++){
+		for(j = 0; j < 100; j++){
+			if(Math.random() > 0.5){
+				ctx.fillRect((i * 2) + 100, (j * 2) + 50, 2, 2);
+			}		
+		}
+
 	}
-
 }
