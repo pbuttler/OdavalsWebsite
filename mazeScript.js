a263697e3b2c9b0c2319a6798156296fdb74106f
pbuttler <peterdbuttler@gmail.com>
2017-12-26 17:22:40 -0700

    added increase and decrease saturation for maze

--- mazeScript.js
+++ mazeScript.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById("mazeCanvas");
 var ctx = canvas.getContext("2d");
+var ratio = 0.5;
 
 function randomizeMaze(){
 	ctx.fillStyle = "#43876e";
@@ -7,10 +8,22 @@ function randomizeMaze(){
 	ctx.fillStyle = "#16112d";
 	for(i = 0; i < 200; i++){
 		for(j = 0; j < 100; j++){
-			if(Math.random() > 0.5){
+			if(Math.random() > ratio){
 				ctx.fillRect((i * 2) + 100, (j * 2) + 50, 2, 2);
 			}		
 		}
 
+	}
+}
+
+function increaseFill(){
+	if(ratio < 1){
+		ratio += .1;
+	}
+}
+
+function decreaseFill(){
+	if(ratio > 0){
+		ratio -= .1;
 	}
 }
